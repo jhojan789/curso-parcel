7a7b22e9ae659d512ded66d1e353274ff8c04b9a
Jhojan Tobón <jhojantobonm@gmail.com>
2023-09-27 15:07:52 -0500

    making fetching to API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 const $app = document.getElementById('app');
 
 const API = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
+
+
+const main = async ()=>{
+  const response = await fetch(API);
+  const products = await response.json();
+
+  const output = products?.map(product=>{
+    return `
+    <article>
+      <img src="${product.images[0]}"/>
+      <h2>
+        ${product.title}
+        <small>Price $ ${product.price}</small>
+      </h2>
+
+    
+    </article>
+    
+    `;
+  }).join('');
+
+  const section = document.createElement('section');
+  section.classList.add('Items');
+  section.innerHTML = output;
+  $app.appendChild(section);
+  
+
+}
+
+main();
